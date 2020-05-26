e2b62555c19940059e8180bf756d99b2734f572c
alyssondepaula <alysson.depaula@outlook.com>
2020-05-26 19:40:38 -0300

    part/chore

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,4 +3,18 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: #87CEEB;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+export const Text = styled.Text`
+color: #a6a6a6;
+font-size: 24px;
+`;
+
+export const TextCreate = styled.Text`
+
+color: #a6a6a6;
+font-size: 16px;
 `;
